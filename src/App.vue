31dd5ee4cf7c9096bf6203575bbd216224bf3495
tim ichiba <ichibat@gmail.com>
2019-11-05 07:22:20 +0900

    Improve

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
+    <Header />
    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
 
   </div>
 </template>
 
 <script>
+import Header from "./components/layout/Header";
 import Todos from "./components/Todos";
 
 export default {
   name: 'app',
   components: {
-    Todos
+    Todos,
+    Header
   },
   data(){
     return {
