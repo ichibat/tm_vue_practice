2187b0d45ce1ff37aa58de34a9fc11c5ab6db704
tim ichiba <ichibat@gmail.com>
2019-11-04 16:22:50 +0900

    Improve

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,12 +1,23 @@
 <template>
-  <div>
-    <p>Todo Item</p>
+  <div class= "todo-item" v-bind:class="{'is-complete': todo.completed}">
+    <p>
+      <input type="checkbox" v-on:change="markComplete">
+      {{todo.title}}
+      <button class="del"></button>
+      
+      </p>
   </div>
 </template>
 
 <script>
 export default {
-  name: "TodoItem"
+  name: "TodoItem",
+  props: ["todo"],
+  methods: {
+    markComplete() {
+      this.todo.completed = !this.todo.completed;
+    }
+  }
 }
 </script>
 
