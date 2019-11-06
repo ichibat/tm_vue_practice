a7b3c9c9bce602283a56d52642aed53a52faa3cb
tim ichiba <ichibat@gmail.com>
2019-11-06 22:29:15 +0900

    Improve

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,7 @@
 import Header from "./components/layout/Header";
 import Todos from "./components/Todos";
 import AddTodo from "./components/AddTodo";
+import axios from 'axios';
 
 export default {
   name: 'app',
@@ -21,23 +22,7 @@ export default {
   },
   data(){
     return {
-      todos: [
-        {
-          id: 1,
-          title: "Todo One",
-          completed: true
-        },
-                {
-          id: 2,
-          title: "Todo Two",
-          completed: true
-        },
-                {
-          id: 3,
-          title: "Todo Three",
-          completed: false
-        }
-      ]
+      todos: []
     }
     },
     methods: {
@@ -47,6 +32,11 @@ export default {
       addTodo(newTodo) {
         this.todos = [...this.todos, newTodo];
       }
+    },
+  created() {
+        axios.get('https://jsonplaceholder.typicode.com/todos?_limit=5')
+          .then(res => this.todos = res.data)
+          // .catch(err => console.log(err));
     }
   }
 </script>
